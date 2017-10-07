51851c4c3fe69ee8a5e942d4fa7fad10d15e9d43
Saidul <tuhinmd718@gmail.com>
2017-10-07 16:19:26 +0600

    Desiginig tuition.html and modify in url

--- TutionFee/urls.py
+++ TutionFee/urls.py
@@ -5,5 +5,5 @@ app_name = 'TutionFee'
 
 urlpatterns = [
     url(r'^Home/$', views.home, name='home'),
-    url(r'^Home/(?P<subject>\w+)$', views.subject_wise, name='subject_wise'),
+    url(r'^Home/(?P<subject>\w+)/(?P<division>\w+)/$', views.subject_wise, name='subject_wise'),
 ]
--- TutionFee/views.py
+++ TutionFee/views.py
@@ -6,16 +6,18 @@ def home(request):
     subject_list = ['CSE', 'LLB', 'BBA', 'EEE', 'ENGLISH',]
     context = {
         'subject_list': subject_list,
+        'All': 'All',
     }
     return render(request, 'TutionFee/home.html', context)
 
 
-def subject_wise(request, subject):
+def subject_wise(request, subject, division):
     divisions_list = ['Dhaka', 'Chittagong', 'Rajshahi', 'Barisal', 'Khulna',
                      'Mymensingh', 'Rangpur', 'Sylhet']
     context = {
         'subject': subject,
         'divisions_list': divisions_list,
+        'division': division,
     }
 
     return render(request, 'TutionFee/tuition.html', context)
